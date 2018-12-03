caff5853f606064d46161f72e7651d9a381b27e0
Holden <zaedrous@gmail.com>
2018-12-03 18:02:41 -0500

    Added PWA

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import IndecisionApp from './components/IndecisionApp';
+import registerServiceWorker from './registerServiceWorker';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+registerServiceWorker();
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path');
 
+/* Import webpack-manifest-plugin */
+const ManifestPlugin = require('webpack-manifest-plugin');
+/* Import sw-precache-webpack-plugin */
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+/* Import copy-webpack-plugin */
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = (env) => {
     const isProduction = env === 'production';
 
@@ -26,6 +33,32 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public')
-        }
+        },
+        plugins: [
+            new ManifestPlugin({
+                fileName: 'asset-manifest.json',
+            }),
+            new SWPrecacheWebpackPlugin({
+                // By default, a cache-busting query parameter is appended to requests
+                // used to populate the caches, to ensure the responses are fresh.
+                // If a URL is already hashed by Webpack, then there is no concern
+                // about it being stale, and the cache-busting can be skipped.
+                dontCacheBustUrlsMatching: /\.\w{8}\./,
+                filename: 'service-worker.js',
+                logger(message) {
+                  if (message.indexOf('Total precache size is') === 0) {
+                    // This message occurs for every build and is a bit too noisy.
+                    return;
+                  }
+                  console.log(message);
+                },
+                minify: true, // minify and uglify the script
+                navigateFallback: '/index.html',
+                staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+            }),
+            new CopyWebpackPlugin([
+                { from: 'src/pwa' }, // define the path of the files to be copied
+            ])
+        ]
     }
 };
